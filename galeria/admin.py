1c968ca95a7afaa0319752207857433573087da2
Thiago Pratti de Mendonça <thiago.mendonca@bsatech.io>
2025-10-14 17:56:16 -0300

    feat: avançando com o admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -6,9 +6,11 @@ from galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
 
-    list_display = ('id', 'nome', 'legenda')
+    list_display = ('id', 'nome', 'legenda', 'data' ,'publicada')
     list_display_links = ('id', 'nome')
     search_fields = ('nome',)
+    list_filter = ("categoria",)
+    list_editable = ("publicada",)
 
 
 admin.site.register(Fotografia, ListandoFotografias)
--- galeria/views.py
+++ galeria/views.py
@@ -5,7 +5,7 @@ from galeria.models import Fotografia
 
 def index(request):
 
-    fotografias = Fotografia.objects.all()
+    fotografias = Fotografia.objects.order_by("data").filter(publicada=True)
 
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
@@ -13,5 +13,5 @@ def imagem(request, foto_id):
 
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
 
-    
+
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
